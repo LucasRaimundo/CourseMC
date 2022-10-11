7b726a201ad2c57dd8602a1471606bdee7ac8d71
Lucas Raimundo <lucasraimundo2010@gmail.com>
2022-10-11 14:55:12 -0300

    Testing the first domain class - Category

--- src/main/java/com/lucasraimundo/coursemc/resources/CategoryResource.java
+++ src/main/java/com/lucasraimundo/coursemc/resources/CategoryResource.java
@@ -1,15 +1,27 @@
 package com.lucasraimundo.coursemc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasraimundo.coursemc.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST FUNCIONANDO";
+	public List<Category> listar() {
+		Category cat1 = new Category(1, "Informatica");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
