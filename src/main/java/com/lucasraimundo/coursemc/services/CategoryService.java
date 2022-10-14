2422ebb579e6b74beb2ef216bac5a8f16721d6f3
Lucas Raimundo <lucasraimundo2010@gmail.com>
2022-10-14 12:54:37 -0300

    Creating repository and service for category

---
+++ src/main/java/com/lucasraimundo/coursemc/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.lucasraimundo.coursemc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasraimundo.coursemc.domain.Category;
+import com.lucasraimundo.coursemc.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repo;
+
+	public Category find(Integer id) {
+		Optional<Category> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
