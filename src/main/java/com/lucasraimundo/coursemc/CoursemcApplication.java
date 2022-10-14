58dc4c4b434ee42edd52733b359444f4cdc60f90
Lucas Raimundo <lucasraimundo2010@gmail.com>
2022-10-14 13:03:26 -0300

    Creating instantiation operation

--- src/main/java/com/lucasraimundo/coursemc/CoursemcApplication.java
+++ src/main/java/com/lucasraimundo/coursemc/CoursemcApplication.java
@@ -1,13 +1,33 @@
 package com.lucasraimundo.coursemc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucasraimundo.coursemc.domain.Category;
+import com.lucasraimundo.coursemc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CoursemcApplication {
+public class CoursemcApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informatica");
+		Category cat2 = new Category(null, "Escritorio");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
