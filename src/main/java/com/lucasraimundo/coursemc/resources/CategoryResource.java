8bde0c8acfb715a1efadaa5c137102177f026232
Lucas Raimundo <lucasraimundo2010@gmail.com>
2022-11-01 19:18:15 -0300

    Inserting a new Category with POST

--- src/main/java/com/lucasraimundo/coursemc/resources/CategoryResource.java
+++ src/main/java/com/lucasraimundo/coursemc/resources/CategoryResource.java
@@ -1,11 +1,15 @@
 package com.lucasraimundo.coursemc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucasraimundo.coursemc.domain.Category;
 import com.lucasraimundo.coursemc.services.CategoryService;
@@ -23,5 +27,12 @@ public class CategoryResource {
 		Category obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
