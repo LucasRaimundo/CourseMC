e372f41a1e96c7a753cf61de4a33c1fa80ac9bd3
Lucas Raimundo <lucasraimundo2010@gmail.com>
2022-11-25 12:56:12 -0300

    Authorizing endpoints for specific profiles

--- src/main/java/com/lucasraimundo/coursemc/config/SecutiryConfig.java
+++ src/main/java/com/lucasraimundo/coursemc/config/SecutiryConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,6 +26,7 @@ import com.lucasraimundo.coursemc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
@@ -44,8 +46,14 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHES_GET = {
 			
 			"/products/**",
-			"/categories/**",
+			"/categories/**"
+			
+	};
+	
+private static final String[] PUBLIC_MATCHES_POST = {
+			
 			"/clients/**"
+			
 	};
 	
 	@Override
@@ -57,6 +65,7 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
+		.antMatchers(HttpMethod.POST, PUBLIC_MATCHES_POST).permitAll()
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHES_GET).permitAll()
 		.antMatchers(PUBLIC_MATCHES).permitAll()
 		.anyRequest().authenticated();
