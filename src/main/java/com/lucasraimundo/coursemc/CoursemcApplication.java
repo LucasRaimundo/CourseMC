5ef7acaea0a74a5ff303a9b40a0d36c368060b2e
Lucas Raimundo <lucasraimundo2010@gmail.com>
2023-01-04 13:41:09 -0300

    Saving first file to S3

--- src/main/java/com/lucasraimundo/coursemc/CoursemcApplication.java
+++ src/main/java/com/lucasraimundo/coursemc/CoursemcApplication.java
@@ -1,13 +1,17 @@
 package com.lucasraimundo.coursemc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucasraimundo.coursemc.services.S3Service;
+
 @SpringBootApplication
 public class CoursemcApplication implements CommandLineRunner {
 	
-	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
@@ -16,7 +20,7 @@ public class CoursemcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		
+		s3Service.uploadFile("C:\\Users\\lucas\\Desktop\\Photos\\Ilume\\test.jpg");
 	}
 
 }
