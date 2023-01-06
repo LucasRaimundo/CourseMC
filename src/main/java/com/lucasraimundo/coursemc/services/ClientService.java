ef4b52dbc7e1297a8afcf572f02f95ef5110a154
Lucas Raimundo <lucasraimundo2010@gmail.com>
2023-01-06 15:21:15 -0300

    adjusting image size

--- src/main/java/com/lucasraimundo/coursemc/services/ClientService.java
+++ src/main/java/com/lucasraimundo/coursemc/services/ClientService.java
@@ -51,6 +51,9 @@ public class ClientService {
 	
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	public Client find(Integer id) {
 		
@@ -130,9 +133,11 @@ public class ClientService {
 		
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		
 		
-		URI uri = s3Service.uploadFile(multipartFile);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
